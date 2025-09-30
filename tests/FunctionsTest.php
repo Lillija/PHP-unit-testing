d4224dee459a29ae456d98d1eb92fb0a70664564
Lillija <asikeligalillija@gmail.com>
2025-09-30 15:37:09 +0300

    idk more

--- tests/FunctionsTest.php
+++ tests/FunctionsTest.php
@@ -6,8 +6,15 @@ use PHPUnit\Framework\TestCase;
 
 require dirname(__DIR__) . '/lib/functions.php';
 
-final class FunctionsTest extends TestCase{
-    public function testAddIntegers():void{
-        $this->assertSame(5, addIntegers(2,3));
+final class FunctionsTest extends TestCase
+{
+    public function testAddTwoPositiveIntegers(): void
+    {
+        $this->assertSame(5, addIntegers(2, 3));
+    }
+
+    public function testAddTwoNegativeIntegers(): void
+    {
+        $this->assertSame(-5, addIntegers(-2, -3));
     }
 }
