b14edcd8bf2600500188b22dee1814adfadd5357
Lillija <asikeligalillija@gmail.com>
2025-09-30 15:06:54 +0300

    PHP unit testing

---
+++ tests/ExampleTest.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+final class ExampleTest extends TestCase
+{
+    public function testTwoValuesAreTheSame(): void{
+        $this->assertSame(1,2);
+    }
+}
